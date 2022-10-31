dc480154b9c229652f967a63d7a25bf79fa480c4
Dafhi <mdafhimayrizki002@mhs.mdp.ac.id>
2022-10-31 21:06:39 +0700

    uts

---
+++ app/src/main/java/com/example/uts_2024240090_mdafhimayrizkiy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.uts_2024240090_mdafhimayrizkiy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
